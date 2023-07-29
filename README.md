2e634b37d0ac918bb7bedc19de3a589ab6ceea88
adey-px <aosteph@gmail.com>
2023-07-28 19:11:18 -0500

    Created new nestjs backend api

---
+++ README.md
@@ -0,0 +1,12 @@
+### Johnny Linkedin Clone
+- Built with Angular frontend and Nest.js backend API
+
+### Technology Used
+- TypeScript
+- Angular
+- Ionic
+- Nest.js
+- TypeORM
+- PostgreSQL
+- Cypress
+- Jasmine
---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASS,
+      database: process.env.POSTGRES_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+//
+export class AppModule {}
